db059aacefa549af79cba68a594bfdf5c83543e0
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-02-11 21:32:14 -0600

    Add third solution for day 4

---
+++ Day 04/day04-template_strings.js
@@ -0,0 +1,4 @@
+// template strings documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/template_strings
+var my_template_string = `My name is ${my_name}.
+My id is ${my_id}.
+My address is ${my_address}.`;
