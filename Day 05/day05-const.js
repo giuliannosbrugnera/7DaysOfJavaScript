049ef55cf3597c9f22b8e3c05c11a85d6ed0fb0b
Giulianno Sbrugnera <giulianno.sbrugnera@gmail.com>
2016-02-12 21:29:43 -0600

    Add second solution for day 5

---
+++ Day 05/day05-const.js
@@ -0,0 +1,7 @@
+// const documentation: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/const
+
+// Write your code below this line.
+
+const PI = Math.PI;
+const EULER = Math.E;
+const SQRT2 = Math.SQRT2;
